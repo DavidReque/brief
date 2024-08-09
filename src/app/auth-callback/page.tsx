fec6f6ef1b2e2633445f4b262e04b69180cb0ab3
David Requeno <davidrequeno52@gmail.com>
2024-08-08 22:14:13 -0600

    trpc

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,11 +1,32 @@
+"use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+import { useEffect } from "react";
 
 const Page = () => {
   const router = useRouter();
-
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
-  return <div>page</div>;
+
+  const { data, error, isLoading } = trpc.authCallback.useQuery();
+
+  useEffect(() => {
+    if (data?.success) {
+      // Manejar el éxito aquí
+      router.push(origin ? `/${origin}` : "/dashboard");
+    }
+  }, [data, router, origin]);
+
+  if (isLoading) {
+    return <div>Verificando autenticación...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return <div>Redirigiendo...</div>;
 };
 
 export default Page;
