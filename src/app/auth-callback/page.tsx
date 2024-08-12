bf5915f66ca39d72e7cd4ad1d79f04f5b6868c1a
David Requeno <davidrequeno52@gmail.com>
2024-08-11 21:19:24 -0600

    Creating Database

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 import { useEffect } from "react";
 
 const Page = () => {
@@ -9,24 +10,33 @@ const Page = () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const { data, error, isLoading } = trpc.authCallback.useQuery();
+  const { data, error, isLoading } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
 
   useEffect(() => {
     if (data?.success) {
-      // Manejar el éxito aquí
+      // user is synced to db
       router.push(origin ? `/${origin}` : "/dashboard");
     }
   }, [data, router, origin]);
 
-  if (isLoading) {
-    return <div>Verificando autenticación...</div>;
-  }
+  useEffect(() => {
+    if (error?.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
+    }
+  }, [error, router]);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  return <div>Redirigiendo...</div>;
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
