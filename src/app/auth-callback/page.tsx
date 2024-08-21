e894e5bd93b5b0a63b6684593b71c6241902b3d7
David Requeno <davidrequeno52@gmail.com>
2024-08-20 23:02:50 -0600

    Boton de cargar archivo

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -32,8 +32,8 @@ const Page = () => {
     <div className="w-full mt-24 flex justify-center">
       <div className="flex flex-col items-center gap-2">
         <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="font-semibold text-xl">Setting up your account...</h3>
-        <p>You will be redirected automatically.</p>
+        <h3 className="font-semibold text-xl">Configurando tu cuenta...</h3>
+        <p>Serás redirigido automáticamente.</p>
       </div>
     </div>
   );
