b6d197dcfdfd58f03621f87deb4c415536fa9ded
David Requeno <davidrequeno52@gmail.com>
2024-09-10 21:53:24 -0600

    Mejorando el select

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -94,67 +94,51 @@ const UploadDropzone = () => {
   };
 
   return (
-    <div className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
-      <Dropzone multiple={false} onDrop={handleFileDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps()} className="h-full">
-            <div className="flex items-center justify-center w-full h-full">
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
-                  <p className="mb-2 text-sm text-zinc-700">
-                    <span className="font-semibold">
-                      Click para seleccionar
-                    </span>{" "}
-                    o arrastrar
-                  </p>
-                </div>
-
-                {acceptedFile ? (
-                  <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
-                    <div className="px-3 py-2 h-full grid place-items-center">
-                      <File className="h-4 w-4 text-blue-500" />
-                    </div>
-                    <div className="px-3 py-2 h-full text-sm truncate">
-                      {acceptedFile.name}
-                    </div>
+    <div className="flex flex-col space-y-4 p-4">
+      <div className="border h-48 border-dashed border-gray-300 rounded-lg overflow-hidden">
+        <Dropzone multiple={false} onDrop={handleFileDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <div {...getRootProps()} className="h-full">
+              <div className="flex items-center justify-center w-full h-full">
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                >
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                    <p className="mb-2 text-sm text-zinc-700">
+                      <span className="font-semibold">
+                        Click para seleccionar
+                      </span>{" "}
+                      o arrastrar
+                    </p>
                   </div>
-                ) : null}
 
-                {isUploading ? (
-                  <div className="w-full mt-4 max-w-xs mx-auto">
-                    <Progress
-                      indicatorColor={
-                        uploadProgress === 100 ? "bg-green-500" : ""
-                      }
-                      value={uploadProgress}
-                      className="h-1 w-full bg-zinc-200"
-                    />
-                    {uploadProgress === 100 ? (
-                      <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
-                        <Loader2 className="h-3 w-3 animate-spin" />
-                        Redirigiendo...
+                  {acceptedFile && (
+                    <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                      <div className="px-3 py-2 h-full grid place-items-center">
+                        <File className="h-4 w-4 text-blue-500" />
                       </div>
-                    ) : null}
-                  </div>
-                ) : null}
+                      <div className="px-3 py-2 h-full text-sm truncate">
+                        {acceptedFile.name}
+                      </div>
+                    </div>
+                  )}
 
-                <input
-                  {...getInputProps()}
-                  type="file"
-                  id="dropzone-file"
-                  className="hidden"
-                />
-              </label>
+                  <input
+                    {...getInputProps()}
+                    type="file"
+                    id="dropzone-file"
+                    className="hidden"
+                  />
+                </label>
+              </div>
             </div>
-          </div>
-        )}
-      </Dropzone>
+          )}
+        </Dropzone>
+      </div>
 
-      <div className="my-6">
+      <div className="w-full">
         <Select
           value={selectedArea}
           onValueChange={(value) => setSelectedArea(value)}
@@ -176,11 +160,27 @@ const UploadDropzone = () => {
       {acceptedFile && selectedArea && (
         <Button
           onClick={handleUpload}
-          className="mx-auto"
+          className="w-full"
           disabled={isUploading}
         >
           {isUploading ? "Cargando..." : "Iniciar carga"}
         </Button>
+      )}
+
+      {isUploading && (
+        <div className="w-full">
+          <Progress
+            indicatorColor={uploadProgress === 100 ? "bg-green-500" : ""}
+            value={uploadProgress}
+            className="h-1 w-full bg-zinc-200"
+          />
+          {uploadProgress === 100 && (
+            <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+              <Loader2 className="h-3 w-3 animate-spin" />
+              Redirigiendo...
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
@@ -195,7 +195,7 @@ export default function UploadButton() {
         <Button>Cargar archivo</Button>
       </DialogTrigger>
 
-      <DialogContent className="h-1/2">
+      <DialogContent className="sm:max-w-md">
         <UploadDropzone />
       </DialogContent>
     </Dialog>
