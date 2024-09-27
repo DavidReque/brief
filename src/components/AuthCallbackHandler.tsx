54c4ed8a6b8a6a531bd2e52588e3fea2485157a6
David Requeno <davidrequeno52@gmail.com>
2024-09-26 21:52:07 -0600

    ...

--- src/components/AuthCallbackHandler.tsx
+++ src/components/AuthCallbackHandler.tsx
@@ -3,15 +3,13 @@
 import { trpc } from "@/app/_trpc/client";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
-import LoadingScreen from "./LoadingScreen";
 
 const AuthCallbackHandler = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  // Manejamos la consulta TRPC
-  const { data, error, isLoading } = trpc.authCallback.useQuery(undefined, {
+  const { data, error } = trpc.authCallback.useQuery(undefined, {
     retry: true,
     retryDelay: 500,
   });
@@ -19,21 +17,16 @@ const AuthCallbackHandler = () => {
   useEffect(() => {
     if (data?.success) {
       // Redirigir cuando el usuario se sincroniza con la base de datos
-      router.push(origin ? `${origin}` : "/dashboard");
+      router.push(origin ? `/${origin}` : "/dashboard");
     }
   }, [data, router, origin]);
 
   useEffect(() => {
     if (error?.data?.code === "UNAUTHORIZED") {
       // Redirigir si hay error de autenticación
-      router.push("/");
+      router.push("/sign-in");
     }
   }, [error, router]);
-
-  // Mostrar una pantalla de carga mientras se está obteniendo la respuesta
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
 
   return null;
 };
